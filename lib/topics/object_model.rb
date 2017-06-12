76bbe398397695a5d8ecf1ddbabeec4cb6f360f0
zoroksana <dev.oksana.svyryda@gmail.com>
2017-06-12 10:06:12 +0300

    Create object_model.rb
    Svyryda.Homework.Object_Model

--- lib/topics/object_model.rb
+++ lib/topics/object_model.rb
@@ -2,4 +2,24 @@
 
 # exercise object model
 module ObjectModel
+  module Linux_Friendly
+    def fork_process
+      super
+    end
+
+  end
+
+  class Desktop
+    def fork_process
+      "Parent: allocate memory"
+    end
+    def mine_bitcoins
+      inspect
+    end
+  end
+
+  class Laptop < Desktop
+    prepend Linux_Friendly
+  end
+
 end
